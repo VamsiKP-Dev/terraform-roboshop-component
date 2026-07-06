6d8738df9980ebe7ddac39ec980f9f2032cf7d2d
Vamsi Krishna P <vamsikrishnap@hotmail.com>
2026-07-06 14:07:58 +0530

    Terraform update

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+    
+    ami_id = data.aws_ami.joindevops.id
+    sg_id   = data.aws_ssm_parameter.sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    common_name = "${var.project}-${var.environment}-${var.component}"
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+    common_tags = {
+        Project = "${var.project}"
+        Environment = "${var.environment}"
+        Terraform = "true"
+    }
+}
